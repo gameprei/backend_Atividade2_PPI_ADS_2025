5f65c41a9504ef07b32330ea1f30661d03d97ed0
Luiz Garcia <luizhenrique9474@gmail.com>
2025-09-01 14:39:10 -0300

    atualizando teste.js

--- teste.js
+++ teste.js
@@ -7,5 +7,15 @@ console.log('Curso gravado com sucesso!');
 console.log(curso.toString());
 
 // Verifica se gravou mesmo
-const cursos = await curso.consultar();
-console.log("Cursos no banco:", cursos.map(c => c.toString()).join("\n"));
+//const cursos = await curso.consultar();
+//console.log("Cursos no banco:", cursos.map(c => c.toString()).join("\n"));
+
+curso.nome = "Programação Python Atualizado";
+curso.descricao = "Curso de Algoritmos em Python Atualizado";
+curso.cargaHoraria = 180;
+curso.valor = 1350.00;
+const cursos = await curso.alterar();
+console.log('Curso alterado com sucesso!');
+console.log(curso.toString());
+
+
